37ee809b55880f8d971bad1975ab649154b21aa7
Rezwana Karim <rezwanakarim13@gmail.com>
2024-08-18 01:00:43 +0600

    navbar toggle, responsive small screen

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 document.addEventListener("DOMContentLoaded", function() {
+   
     const filterButtons = document.querySelectorAll('.skill-type .skills-list-type');
     const skills = document.querySelectorAll('.skills-list .skills-list-item');
+
+    
  
     const showAllSkills = () => {
         skills.forEach(skill => {
@@ -32,3 +35,36 @@ document.addEventListener("DOMContentLoaded", function() {
         });
     });
 });
+
+
+const menuIcon = document.querySelector('#menu-icon');
+const navbar = document.querySelector('.navbar');
+const sections = document.querySelectorAll('section');
+const navLinks = document.querySelectorAll('header nav a');
+
+window.onscroll = () => {
+    let current = '';
+
+    sections.forEach(sec => {
+        const top = window.scrollY;
+        const offset = sec.offsetTop - 150;
+        const height = sec.offsetHeight;
+        const id = sec.getAttribute('id');
+
+        if (top >= offset && top < offset + height) {
+            current = id; 
+        }
+    });
+   
+    navLinks.forEach(link => {
+        link.classList.remove('active');
+        if (link.getAttribute('href').includes(current)) {
+            link.classList.add('active');
+        }
+    });
+};
+
+menuIcon.onclick = () => {
+    menuIcon.classList.toggle('bx-x');
+    navbar.classList.toggle('active');
+};
